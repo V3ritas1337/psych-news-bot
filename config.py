14f2abc3c14f286e6a58d725ee4787cfb806c323
unknown <josefflewis97@gmail.com>
2021-10-06 12:12:24 +0100

    update to config & feed parser.

--- config.py
+++ config.py
@@ -1,4 +1,9 @@
 import yaml
 
 with open('dev-config.yaml', 'r') as f:
-  print(yaml.load(f))
+   data = yaml.load(f, Loader=yaml.FullLoader)
+
+PA = data['feeds'][0]['psilocybinalpha']
+TR = data['feeds'][1]['trufflereport']
+CS = data['feeds'][2]['cannabisstock']
+PSW = data['feeds'][3]['psychedelicstockwatch']
--- feed_parser.py
+++ feed_parser.py
@@ -5,12 +5,13 @@ from datetime import date, datetime
 
 def parser(symbol):
     NewsFeed = feedparser.parse(symbol)
-    entry = NewsFeed.entries[1]
+    entry = NewsFeed.entries
     return entry # entry.published_parsed
 
 # TODO: if todays date same as published_parsed, print link :) 
-# print(datetime(*parser(PA)[:6]))
-print(parser(PA).link)
-
+# print(datetime(parser(PA)[:6]))
+print(parser(TR))
+print(parser(TR).published_parsed)
+# https://waylonwalker.com/parsing-rss-python/
 
 # print(date.split(' '))
